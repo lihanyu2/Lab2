91db73458c9914796995c07c749b869cd2bf7c27
lihanyu2 <lihanyu2@illinois.edu>
2018-01-31 16:38:26 -0600

    gegeg

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,9 +34,29 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
-        }
+                if (maze.canMove()) {
+                    maze.move();
+                } else {
+                    maze.turnLeft();
+                }
+                if (maze.canMove()) {
+                    maze.move();
+                } else {
+                    maze.turnRight();
+                    maze.turnRight();
+                }
+                if (maze.canMove()) {
+                    maze.move();
+                } else {
+                    maze.turnRight();
+                    maze.move();
+                }
 
+            if (maze.isFinished()) {
+                System.out.println(step);
+                break;
+            }
+        }
         if (maze.isFinished()) {
             System.out.println("You solved the maze!");
         } else {
